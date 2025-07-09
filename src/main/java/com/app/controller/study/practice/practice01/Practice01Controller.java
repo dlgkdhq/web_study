b5d9a4dde8ea3fdae68e6309fc5fb9404ec16c2f
dlgkdhq <dlgkdms920@naver.com>
2025-07-09 15:07:28 +0900

    연습1  URL 맵핑하기

---
+++ src/main/java/com/app/controller/study/practice/practice01/Practice01Controller.java
@@ -0,0 +1,28 @@
+package com.app.controller.study.practice.practice01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/practice01")
+public class Practice01Controller {
+
+	@RequestMapping("/main")
+	public String main() {
+		return "practice01/main";
+	}
+	
+	//@RequestMapping("/product/mouse")
+	@RequestMapping(value = "/product/mouse", method = RequestMethod.GET)
+	public String mouse() {
+		return "practice01/product/mouse";
+	}
+	
+	//@RequestMapping("/product/keyboard")
+	@GetMapping("/product/keyboard")
+	public String keyboard() {
+		return "practice01/product/keyboard";
+	}
+}
