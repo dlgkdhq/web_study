fc04a7aaaf5fbc4ed14a7bedd929637a43abed6c
dlgkdhq <dlgkdms920@naver.com>
2025-08-27 10:35:05 +0900

    사용자 비밀번호 암호화 처리 추가

--- src/main/java/com/app/service/user/impl/UserServiceImpl.java
+++ src/main/java/com/app/service/user/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.app.service.user.impl;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +12,11 @@ import com.app.dto.user.User;
 import com.app.dto.user.UserSearchCondition;
 import com.app.dto.user.UserValidError;
 import com.app.service.user.UserService;
+import com.app.util.SHA256Encryptor;
 import com.app.validator.UserCustomValidator;
 
 import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @Service
 public class UserServiceImpl implements UserService {
@@ -29,28 +32,38 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public int saveAdminUser(User user) {
-		
-		//관리자 계정 추가할 때 필요한 체크 로직
-		//user.setUserType("ADM");
+
+		// 관리자 계정 추가할 때 필요한 체크 로직
+		// user.setUserType("ADM");
 		user.setUserType(CommonCode.USER_USERTYPE_ADMIN);
 		int result = userDAO.saveUser(user);
 		log.info("savaAdminUser 관리자 계정 추가 : {}", user);
-		
+
 		return result;
 	}
 
 	@Override
 	public int saveCustomerUser(User user) {
-		
-		//사용자 계정 추가할 때 필요한 체크 로직
-		
-		//UserValidError userValidError = new UserValidError();
-		//if(UserCustomValidator.validate(user, userValidError) == false) {
-		
-		//user.setUserType("CUS");
+
+		// 사용자 계정 추가할 때 필요한 체크 로직
+
+		// UserValidError userValidError = new UserValidError();
+		// if(UserCustomValidator.validate(user, userValidError) == false) {
+
+		// user.setUserType("CUS");
 		user.setUserType(CommonCode.USER_USERTYPE_CUSTOMER);
-		int result = userDAO.saveUser(user);
-		
+
+		// 비밀번호 암호화
+		try {
+			String encPw = SHA256Encryptor.encrypt(user.getPw());
+			user.setPw(encPw);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		int result = userDAO.saveUser(user); // 암호화 된 비밀번호가 저장됨
+
 		return result;
 	}
 
@@ -68,36 +81,45 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public User checkUserLogin(User user) {
-		//매개변수로 들어온 User 객체에 id, pw, userType 로그인을 처리할 수 있는 정상적인 값을 가지고 있는지 여부 체크
-		//메소드 주요 로직 -> 로그인 처리 성공? 실패?
-		
-		//id pw 일치하는가?
-		
+		// 매개변수로 들어온 User 객체에 id, pw, userType 로그인을 처리할 수 있는 정상적인 값을 가지고 있는지 여부 체크
+		// 메소드 주요 로직 -> 로그인 처리 성공? 실패?
+
+		// id pw 일치하는가?
+
 		/*
-		// 케이스 1) DB에서 정보를 조회 한 후 -> Service Layer 상태에서 비교하는 로직을 수행! 
-		
-		//사용자 정보를 조회해서, id pw 맞나 확인
-		User loginUser = userDAO.findUserById(user.getId());
-		
-		// if(loginUser != null)		
-		if( loginUser != null && loginUser.getPw().equals(user.getPw())
-					&& loginUser.getUserType().equals(user.getUserType()) ) {
-			return loginUser;
-		}
-		// checkUserLogin 메소드 호출 -> return null? id,pw 틀렸다
-		//   return user객체 ? -> 맞다!
-			
-		return null;
-		*/
-		
-		/*
-			return 의미가 담긴 코드 (SUC, FAL, LCK)...
-			int 숫자 return -> 1:성공 2:id는 맞는데 비번이 틀렸다 3:아이디없다
-							 4:신고로잠겼다 5: 휴면계정 6:...
+		 * // 케이스 1) DB에서 정보를 조회 한 후 -> Service Layer 상태에서 비교하는 로직을 수행!
+		 * 
+		 * //사용자 정보를 조회해서, id pw 맞나 확인 User loginUser =
+		 * userDAO.findUserById(user.getId());
+		 * 
+		 * // if(loginUser != null) if( loginUser != null &&
+		 * loginUser.getPw().equals(user.getPw()) &&
+		 * loginUser.getUserType().equals(user.getUserType()) ) { return loginUser; }
+		 * 
+		 * // 암호화 처리가 되어 있으면? // loginUser.getPw().equals( user.getPw() ) //
+		 * loginUser.getPw().equals( SHA256Encryptor.encrypt( user.getPw() ) )
+		 * 
+		 * // checkUserLogin 메소드 호출 -> return null? id,pw 틀렸다 // return user객체 ? -> 맞다!
+		 * 
+		 * return null;
 		 */
-		
+
+		/*
+		 * return 의미가 담긴 코드 (SUC, FAL, LCK)... int 숫자 return -> 1:성공 2:id는 맞는데 비번이 틀렸다
+		 * 3:아이디없다 4:신고로잠겼다 5: 휴면계정 6:...
+		 */
+
 		// 케이스 2) DB에서 쿼리를 통해, 정상여부 체크 로직 수행
 		// userDAO.checkUserLogin -> sql query 상에서 id, pw, userType 동일한 경우를 체크
+
+		// 비밀번호 암호화 과정
+		try {
+			String encPw = SHA256Encryptor.encrypt(user.getPw());
+			user.setPw(encPw);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		User loginUser = userDAO.checkUserLogin(user);
 
 		return loginUser; // 조회:O 객체, 조회:X null
@@ -123,14 +145,14 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public boolean isDuplicatedId(String id) {
-		User user = userDAO.findUserById(id);	//해당 ID로 DB에서 조회
-		
-		if(user == null) {	//해당 아이디 객체가 없다 -> id값 사용 안한다 -> 중복 xxx
+		User user = userDAO.findUserById(id); // 해당 ID로 DB에서 조회
+
+		if (user == null) { // 해당 아이디 객체가 없다 -> id값 사용 안한다 -> 중복 xxx
 			return false;
-		} else {	//아이디 객체가 잆다 -> id 값이 pk로 사용되고 있다 -> 중복 ooo
+		} else { // 아이디 객체가 잆다 -> id 값이 pk로 사용되고 있다 -> 중복 ooo
 			return true;
 		}
 
 	}
-	
+
 }
